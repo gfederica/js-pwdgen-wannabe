6fdb1ea8f7f87e89cfe6007262a24c854bb91a7f
gfederica <effegiudice@gmail.com>
2021-04-26 16:58:27 +0200

    aggiunta funzione lowercase - completo

--- js/myscript.js
+++ js/myscript.js
@@ -1,5 +1,10 @@
-var nome = prompt("Come ti chiami?");
-var cognome = prompt("Qual è il tuo cognome?");
-var colore = prompt("Qual è il tuo colore preferito?")
+
+var nomeInput = prompt("Come ti chiami?");
+var cognomeInput = prompt("Qual è il tuo cognome?");
+var coloreInput = prompt("Qual è il tuo colore preferito?");
+
+var nome = nomeInput.toLowerCase();
+var cognome = cognomeInput.toLowerCase();
+var colore = coloreInput.toLowerCase();
 
 document.getElementById('password').innerHTML = nome + cognome + colore + 21;
