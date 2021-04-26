6a89815eb85feef95815a89e5e7b4e5b554cf173
gfederica <effegiudice@gmail.com>
2021-04-26 16:50:41 +0200

    completato + bonus

---
+++ js/myscript.js
@@ -0,0 +1,5 @@
+var nome = prompt("Come ti chiami?");
+var cognome = prompt("Qual è il tuo cognome?");
+var colore = prompt("Qual è il tuo colore preferito?")
+
+document.getElementById('password').innerHTML = nome + cognome + colore + 21;
